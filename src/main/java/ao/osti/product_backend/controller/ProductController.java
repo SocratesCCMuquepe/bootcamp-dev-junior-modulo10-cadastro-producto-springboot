38d585c11d7f88842ff78407d881c095852b47b4
Socrates Muquepe <socycrates@hotmail.com>
2024-12-23 07:19:38 +0100

    Adicionando o endpoint para update de productos

--- src/main/java/ao/osti/product_backend/controller/ProductController.java
+++ src/main/java/ao/osti/product_backend/controller/ProductController.java
@@ -14,10 +14,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
 @RestController
 @CrossOrigin
@@ -50,6 +52,32 @@ public class ProductController {
     @GetMapping("products")
     public List<Product> getProducts() {
         return productRepository.findAll();
+    }
+
+    @DeleteMapping("products/{id}")
+    public ResponseEntity<Void> removeProduct(@PathVariable int id) {
+        Product product = productRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Product not found..!"));
+
+        productRepository.delete(product);
+
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("products/{id}")
+    public ResponseEntity<Product> updateProduct(@PathVariable int id, @RequestBody Product updateProduct) {
+        Product product = productRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Product not found..!"));
+
+        product.setName(updateProduct.getName());
+        product.setDescription(updateProduct.getDescription());
+        product.setPrice(updateProduct.getPrice());
+        product.setNewProduct(updateProduct.isNewProduct());
+        product.setPromotion(updateProduct.isPromotion());
+
+        productRepository.save(product);
+
+        return ResponseEntity.ok().build();
     }
 
 }
